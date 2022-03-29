40ceb4faa094f11cb26db59eaef5a3d109dd1846
gabriel velez <gabriel.dan.velez@gmail.com>
2022-03-29 00:39:38 -0400

    Added functionality

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+
+import { Link } from "react-router-dom";
 
 export default function MovieList(props) {
   return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
+    <div className='movie-list'>
+      {props.movies.map((movie) => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -12,16 +14,19 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
+  if (props === undefined) return "Getting your movie...";
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
+    <div className='movie-card'>
+      <Link to={`/movies/${props.movie.id}`}>
+        <h2>{title}</h2>
+        <div className='movie-director'>
+          Director: <em>{director}</em>
+        </div>
+        <div className='movie-metascore'>
+          Metascore: <strong>{metascore}</strong>
+        </div>
+      </Link>
     </div>
   );
 }
